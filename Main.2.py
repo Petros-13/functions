13648b12db29d025f1d40a321eab1f85cebf2f69
Petros-13 <petroskalkounos78@gmail.com>
2025-08-15 15:28:21 +0200

    2nd commit

---
+++ Main.2.py
@@ -0,0 +1,8 @@
+def weather(location,season):
+    print("Welcome to my weather desciption")
+    print(location,"is not at",season)
+
+weather("Greece","Summer")
+weather("Kenya","Autumn")
+weather("Australia","Winter")
+weather("USA","Spring")
